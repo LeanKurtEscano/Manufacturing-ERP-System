e38b60eb1ae4e4bed6eab11f7009733e5305649f
Lean Kurt <leankurtescano@gmail.com>
2025-07-28 23:43:37 +0800

    Initialize Project

---
+++ backend/manufacturing-erp/src/test/java/com/leankurt/erp/manufacturing_erp/ManufacturingErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leankurt.erp.manufacturing_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManufacturingErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
