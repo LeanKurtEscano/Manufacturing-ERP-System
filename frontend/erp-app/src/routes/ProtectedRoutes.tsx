dcd54ec4a47424ce31b1673e36a63fa68952e4f3
Lean Kurt <leankurtescano@gmail.com>
2025-07-30 18:23:12 +0800

    Added Role base access control setup

---
+++ frontend/erp-app/src/routes/ProtectedRoutes.tsx
@@ -0,0 +1,18 @@
+import type { JSX } from "react";
+import { Navigate } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  element: JSX.Element;
+  allowedRoles: string[];
+}
+
+const ProtectedRoute = ({ element, allowedRoles }: ProtectedRouteProps) => {
+
+  const userRole = localStorage.getItem("userRole");
+  if (!userRole || !allowedRoles.includes(userRole || "")) {
+    return <Navigate to="/unauthorized" />;
+  }
+  return element;
+};
+
+export default ProtectedRoute;
